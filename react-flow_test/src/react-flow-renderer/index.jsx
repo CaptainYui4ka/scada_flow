f2e9fc7d684d1bc91279b0f98946d747f13ee9ff
Dinar <yuichkacaptain@gmail.com>
2022-04-28 11:37:09 +0500

    update

--- react-flow_test/src/react-flow-renderer/index.jsx
+++ react-flow_test/src/react-flow-renderer/index.jsx
@@ -43,77 +43,77 @@ const ReactFlowRenderer = () => {
 
     const connectHandler = (params) => {
         setElements((els) =>
-        addEdge({ ...params, type: 'smoothstep',style:{ stroke: '#FF0000'}, animated: true }, els))
+            addEdge({ ...params, type: 'smoothstep', style: { stroke: '#FF0000' }, animated: true }, els))
     };
-    
+
     const addRectangleHandler = () => {
         const newNode = {
-          id: `${Date.now()}`,
-          data: { label: `${name}` },
-          type: "rectangle",
-          position: {
-            x: Math.random() * window.innerWidth,
-            y: Math.random() * window.innerHeight
-          }
+            id: `${Date.now()}`,
+            data: { label: `${name}` },
+            type: "rectangle",
+            position: {
+                x: Math.random() * window.innerWidth,
+                y: Math.random() * window.innerHeight
+            }
         };
         newNode.data = { ...newNode.data, id: `${newNode.id}` };
-    
+
         setElements((prev) => {
-          return [...prev, newNode];
+            return [...prev, newNode];
         });
         setName("");
     };
 
     const addCircleHandler = () => {
         const newNode = {
-          id: `${Date.now()}`,
-          data: { label: `${name}` },
-          type: "circle",
-          position: {
-            x: Math.random() * window.innerWidth,
-            y: Math.random() * window.innerHeight
-          }
+            id: `${Date.now()}`,
+            data: { label: `${name}` },
+            type: "circle",
+            position: {
+                x: Math.random() * window.innerWidth,
+                y: Math.random() * window.innerHeight
+            }
         };
         newNode.data = { ...newNode.data, id: `${newNode.id}` };
-    
+
         setElements((prev) => {
-          return [...prev, newNode];
+            return [...prev, newNode];
         });
         setName("");
     };
 
     const addTriangleHandler = () => {
         const newNode = {
-          id: `${Date.now()}`,
-          data: { label: `${name}` },
-          type: "triangle",
-          position: {
-            x: Math.random() * window.innerWidth,
-            y: Math.random() * window.innerHeight
-          }
+            id: `${Date.now()}`,
+            data: { label: `${name}` },
+            type: "triangle",
+            position: {
+                x: Math.random() * window.innerWidth,
+                y: Math.random() * window.innerHeight
+            }
         };
         newNode.data = { ...newNode.data, id: `${newNode.id}` };
         setElements((prev) => {
-          return [...prev, newNode];
+            return [...prev, newNode];
         });
         setName("");
-        
+
     };
 
     const addTextHandler = () => {
         const newNode = {
-          id: `${Date.now()}`,
-          data: { label: `${name}` },
-          type: "text",
-          position: {
-            x: 0,
-            y: 0
-          }
+            id: `${Date.now()}`,
+            data: { label: `${name}` },
+            type: "text",
+            position: {
+                x: 0,
+                y: 0
+            }
         };
         newNode.data = { ...newNode.data, id: `${newNode.id}` };
-    
+
         setElements((prev) => {
-          return [...prev, newNode];
+            return [...prev, newNode];
         });
         setName("");
     };
@@ -132,21 +132,21 @@ const ReactFlowRenderer = () => {
                 setActiveNode(_current);
             }
         });
-    // setNewName(activeNode.data.label)
+        // setNewName(activeNode.data.label)
     };
 
     const updateNodeHandler = () => {
         if (!activeNode) return;
         setElements(
-          elements.map((_current) => {
-            if (_current.id === activeNode.id) {
-              return {
-                ..._current,
-                data: { label: newName, id: _current.data.id }
-              };
-            }
-            return _current;
-          })
+            elements.map((_current) => {
+                if (_current.id === activeNode.id) {
+                    return {
+                        ..._current,
+                        data: { label: newName, id: _current.data.id }
+                    };
+                }
+                return _current;
+            })
         );
     };
 
@@ -156,57 +156,61 @@ const ReactFlowRenderer = () => {
         reactFlowInstance.fitView();
     };
 
+    socket.onopen = () => {
+        console.log('подключено');
+    }
+
+    /*
+    socket.onmessage = (event) => {
+        console.log('есть сообщение')
+        const lineSignal = JSON.parse(event.data);
+        console.log(lineSignal);
+        let linedate = event.data;
+        console.log(linedate);
+        let json = JSON.stringify(linedate);
+        console.log(json);
+        let fileName = 'signal.json'
+
+        let fileToSave = new Blob([JSON.stringify(json, null, 4)], {
+            type: 'application/json',
+            name: fileName
+        })
+        saveAs (fileToSave, fileName);
+    }
+    */
+
+    function myMessage(event) {
+        let message = event.data;
+
+        console.log('есть сообщение')
+        const lineSignal = JSON.parse(message);
+        console.log(lineSignal);
+    }
+    socket.onmessage = myMessage;
     
-        socket.onopen = () => {
-            console.log('подключено');
-        }
-        /*
-        socket.onmessage = (event) => {
-            console.log('есть сообщение')
-            const lineSignal = JSON.parse(event.data);
-            console.log(lineSignal);
-            let linedate = event.data;
-            console.log(linedate);
-            let json = JSON.stringify(linedate);
-            console.log(json);
-            let fileName = 'signal.json'
 
-            let fileToSave = new Blob([JSON.stringify(json, null, 4)], {
-                type: 'application/json',
-                name: fileName
-            })
-            saveAs (fileToSave, fileName);
-        }
-        */
-        function myMessage(event) {
-            console.log('есть сообщение')
-            const lineSignal = JSON.parse(event.data);
-            console.log(lineSignal);
-        }
-        socket.onmessage = myMessage;
+    //save pos
+    const saveChangesHandler = () => {
+        console.log("state", instance.getElements());
+        //сохранение в локальное хранилище
+        localStorage.setItem('Position', JSON.stringify(instance.getElements()));
+        //отправка json
+        let pos = JSON.stringify(instance.getElements());
+        socket.send(pos);
+    }
 
-        //save pos
-        const saveChangesHandler = () => {
-            console.log("state", instance.getElements());
-            //сохранение в локальное хранилище
-            localStorage.setItem('Position', JSON.stringify(instance.getElements()));
-            //отправка json
-            let pos = JSON.stringify(instance.getElements());
-            socket.send(pos);
+    socket.onclose = function (event) {
+        if (event.wasClean) {
+            console.log('Соединение закрыто чисто')
+        } else {
+            console.log('Обрыв соединения')
         }
+        console.log('Код: ' + event.code + ' причина: ' + event.reason)
+    }
 
-        socket.onclose = function(event) {
-            if (event.wasClean) {
-              console.log('Соединение закрыто чисто')
-            } else {
-              console.log('Обрыв соединения')
-            }
-            console.log('Код: ' + event.code + ' причина: ' + event.reason)
-        }
-
-        socket.onerror = function(event) {
-            console.log(event);
-        }
+    socket.onerror = function (event) {
+        console.log(event);
+    }
 
 
     return (
@@ -218,85 +222,85 @@ const ReactFlowRenderer = () => {
                 marginLeft: "12.5vw"
             }}
         >
-        <ReactFlow
-            elements={elements}
-            onElementsRemove={elementRemoveHandler}
-            onConnect={connectHandler}
-            deleteKeyCode={8 || 46}
-            onEdgeUpdate={edgeUpdateHandler}
-            nodeTypes={nodeTypes}
-            snapToGrid={true}
-            snapGrid={[16, 16]}
-            connectionLineStyle={{ stroke: "black", strokeWidth: 2 }}
-            onDoubleClick={clickHandler}
-            onLoad={onLoad}
-        >
-            <Background variant="dots" gap={15} size={2} color="#c8c8c8" />
+            <ReactFlow
+                elements={elements}
+                onElementsRemove={elementRemoveHandler}
+                onConnect={connectHandler}
+                deleteKeyCode={8 || 46}
+                onEdgeUpdate={edgeUpdateHandler}
+                nodeTypes={nodeTypes}
+                snapToGrid={true}
+                snapGrid={[16, 16]}
+                connectionLineStyle={{ stroke: "black", strokeWidth: 2 }}
+                onDoubleClick={clickHandler}
+                onLoad={onLoad}
+            >
+                <Background variant="dots" gap={15} size={2} color="#c8c8c8" />
 
-            <MiniMap
-                nodeColor={(node) => {
-                    switch (node.type) {
-                        case "rectangle":
-                            return "red";
-                        case "circle":
-                            return "#00ff00";
-                        case "triangle":
-                            return "rgb(0,0,255)";
-                        default:
-                            return "#eee";
-                    }   
-                }}
-            />
+                <MiniMap
+                    nodeColor={(node) => {
+                        switch (node.type) {
+                            case "rectangle":
+                                return "red";
+                            case "circle":
+                                return "#00ff00";
+                            case "triangle":
+                                return "rgb(0,0,255)";
+                            default:
+                                return "#eee";
+                        }
+                    }}
+                />
 
-            <Controls />
-        </ReactFlow>
+                <Controls />
+            </ReactFlow>
 
-        <div>
-            <input
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                type="text"
-                placeholder="Enter new node name"
-            />
+            <div>
+                <input
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    type="text"
+                    placeholder="Enter new node name"
+                />
 
-            <button type="button" onClick={addRectangleHandler}>
-                Create Rectangle
-            </button>
+                <button type="button" onClick={addRectangleHandler}>
+                    Create Rectangle
+                </button>
 
-            <button type="button" onClick={addCircleHandler}>
-                Create Circle
-            </button>
+                <button type="button" onClick={addCircleHandler}>
+                    Create Circle
+                </button>
 
-            <button type="button" onClick={addTriangleHandler}>
-                Create Triangle
-            </button>
+                <button type="button" onClick={addTriangleHandler}>
+                    Create Triangle
+                </button>
 
-            <button type="button" onClick={addTextHandler}>
-                Plain text
-            </button>
+                <button type="button" onClick={addTextHandler}>
+                    Plain text
+                </button>
+            </div>
+
+            <div>
+                <input
+                    value={newName}
+                    onChange={(e) => setNewName(e.target.value)}
+                    type="text"
+                />
+
+                <button type="button" onClick={updateNodeHandler}>
+                    Обновить состояние фигуры
+                </button>
+            </div>
+            <div>
+                <button type="button" onClick={saveChangesHandler}>
+                    Сохранение
+                </button>
+                <button type="button" onClick={getPosition}>
+                    Загрузка
+                </button>
+                {/* Сделать кнопку которая открывает модальное окно modal.jsx */}
+            </div>
         </div>
-
-        <div>
-            <input
-                value={newName}
-                onChange={(e) => setNewName(e.target.value)}
-                type="text"
-            />
-
-            <button type="button" onClick={updateNodeHandler}>
-                Обновить состояние фигуры
-            </button>
-        </div>
-        <div>
-            <button type="button" onClick={saveChangesHandler}>
-                Сохранение
-            </button>
-            <button type="button" onClick={getPosition}>
-                Загрузка
-            </button>
-            {/* Сделать кнопку которая открывает модальное окно modal.jsx */}
-        </div>
-    </div>
     );
 };
 
--- react-flow_test/src/react-flow-renderer/line.jsx
+++ react-flow_test/src/react-flow-renderer/line.jsx
@@ -1,70 +1,82 @@
 import React from 'react'
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-  } from 'chart.js';
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 import { Line } from 'react-chartjs-2';
 
+const socket = new WebSocket('ws://localhost:5000/', "protocolOne");
+
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 export const options = {
-    responsive: true,
-    interaction: {
-      mode: 'index',
-      intersect: false,
+  responsive: true,
+  interaction: {
+    mode: 'index',
+    intersect: false,
+  },
+  plugins: {
+    legend: {
+      position: 'top',
     },
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      title: {
-        display: true,
-        text: 'Line Chart',
-      },
+    title: {
+      display: true,
+      text: 'Line Chart',
     },
+  },
 };
 const labels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+
+function myMessage(event) {
+  let message = event.data;
+
+  console.log('есть сообщение')
+  const lineSignal = JSON.parse(message);
+  console.log(lineSignal);
+}
+socket.onmessage = myMessage;
+console.log();
 
 export const data = {
   labels,
   datasets: [
     {
       label: 'Dataset 1',
-      data: [-5,20,5,35,5,5,10,20,15,45],
+      data: [50, 25, 10, 5, 0, -10, -20, -50, -25, -50],
       borderColor: 'blue',
       backgroundColor: '#6BA7FF'
     },
     {
       label: 'Dataset 2',
-      data: [-20,-15,-10,-5,0,0,10,5,25,10],
+      data: [-20, -15, -10, -5, 0, 0, 10, 5, 25, 10],
       borderColor: 'red',
       backgroundColor: '#FF6B6B'
     },
-  ],  
+  ],
 };
 
 
 const LineChart = () => {
-    return (
-        <div style={{width: 700}}>
-            <Line options={options} data={data}/>
-            
-        </div>
-    )
+  return (
+    <div style={{ width: 700 }}>
+      <Line options={options} data={data} />
+
+    </div>
+  )
 }
 
 export default LineChart;
